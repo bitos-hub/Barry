89ae708167db81762e1f90c069b4450bf717f772
Franchesco08 <a20221148@pucp.edu.pe>
2024-10-03 20:37:35 -0500

    Interfaz Ingresar Horario

---
+++ Interfaz/AgregarHorarios.h
@@ -0,0 +1,214 @@
+#pragma once
+
+namespace Interfaz {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Resumen de AgregarHorarios
+	/// </summary>
+	public ref class AgregarHorarios : public System::Windows::Forms::Form
+	{
+	public:
+		AgregarHorarios(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: agregar código de constructor aquí
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Limpiar los recursos que se estén usando.
+		/// </summary>
+		~AgregarHorarios()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::PictureBox^ pictureBox1;
+	protected:
+	private: System::Windows::Forms::PictureBox^ pictureBox2;
+	private: System::Windows::Forms::Label^ agreg;
+	private: System::Windows::Forms::DataGridView^ dataGridView1;
+	private: System::Windows::Forms::DataGridViewTextBoxColumn^ Horario;
+	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::TextBox^ txt_horario;
+	private: System::Windows::Forms::Button^ btn_agregar;
+	private: System::Windows::Forms::Button^ btn_modificar;
+	private: System::Windows::Forms::Button^ btn_eliminar;
+
+
+	private:
+		/// <summary>
+		/// Variable del diseñador necesaria.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Método necesario para admitir el Diseñador. No se puede modificar
+		/// el contenido de este método con el editor de código.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(AgregarHorarios::typeid));
+			System::Windows::Forms::DataGridViewCellStyle^ dataGridViewCellStyle1 = (gcnew System::Windows::Forms::DataGridViewCellStyle());
+			System::Windows::Forms::DataGridViewCellStyle^ dataGridViewCellStyle2 = (gcnew System::Windows::Forms::DataGridViewCellStyle());
+			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
+			this->pictureBox2 = (gcnew System::Windows::Forms::PictureBox());
+			this->agreg = (gcnew System::Windows::Forms::Label());
+			this->dataGridView1 = (gcnew System::Windows::Forms::DataGridView());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->txt_horario = (gcnew System::Windows::Forms::TextBox());
+			this->btn_agregar = (gcnew System::Windows::Forms::Button());
+			this->btn_modificar = (gcnew System::Windows::Forms::Button());
+			this->btn_eliminar = (gcnew System::Windows::Forms::Button());
+			this->Horario = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox2))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->BeginInit();
+			this->SuspendLayout();
+			// 
+			// pictureBox1
+			// 
+			this->pictureBox1->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox1.Image")));
+			this->pictureBox1->Location = System::Drawing::Point(405, 12);
+			this->pictureBox1->Name = L"pictureBox1";
+			this->pictureBox1->Size = System::Drawing::Size(93, 128);
+			this->pictureBox1->SizeMode = System::Windows::Forms::PictureBoxSizeMode::StretchImage;
+			this->pictureBox1->TabIndex = 17;
+			this->pictureBox1->TabStop = false;
+			// 
+			// pictureBox2
+			// 
+			this->pictureBox2->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox2.Image")));
+			this->pictureBox2->Location = System::Drawing::Point(22, 26);
+			this->pictureBox2->Name = L"pictureBox2";
+			this->pictureBox2->Size = System::Drawing::Size(48, 50);
+			this->pictureBox2->SizeMode = System::Windows::Forms::PictureBoxSizeMode::StretchImage;
+			this->pictureBox2->TabIndex = 18;
+			this->pictureBox2->TabStop = false;
+			// 
+			// agreg
+			// 
+			this->agreg->AutoSize = true;
+			this->agreg->Font = (gcnew System::Drawing::Font(L"Gill Sans Ultra Bold", 18, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->agreg->Location = System::Drawing::Point(76, 38);
+			this->agreg->Name = L"agreg";
+			this->agreg->Size = System::Drawing::Size(265, 33);
+			this->agreg->TabIndex = 19;
+			this->agreg->Text = L"Agregar Horario";
+			// 
+			// dataGridView1
+			// 
+			dataGridViewCellStyle1->Alignment = System::Windows::Forms::DataGridViewContentAlignment::MiddleCenter;
+			dataGridViewCellStyle1->BackColor = System::Drawing::SystemColors::Control;
+			dataGridViewCellStyle1->Font = (gcnew System::Drawing::Font(L"Yu Gothic Medium", 9.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			dataGridViewCellStyle1->ForeColor = System::Drawing::SystemColors::WindowText;
+			dataGridViewCellStyle1->SelectionBackColor = System::Drawing::SystemColors::Highlight;
+			dataGridViewCellStyle1->SelectionForeColor = System::Drawing::SystemColors::HighlightText;
+			dataGridViewCellStyle1->WrapMode = System::Windows::Forms::DataGridViewTriState::True;
+			this->dataGridView1->ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+			this->dataGridView1->ColumnHeadersHeightSizeMode = System::Windows::Forms::DataGridViewColumnHeadersHeightSizeMode::AutoSize;
+			this->dataGridView1->Columns->AddRange(gcnew cli::array< System::Windows::Forms::DataGridViewColumn^  >(1) { this->Horario });
+			this->dataGridView1->Location = System::Drawing::Point(44, 181);
+			this->dataGridView1->Name = L"dataGridView1";
+			this->dataGridView1->Size = System::Drawing::Size(297, 228);
+			this->dataGridView1->TabIndex = 20;
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Font = (gcnew System::Drawing::Font(L"Yu Gothic Medium", 12, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label1->Location = System::Drawing::Point(40, 97);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(241, 21);
+			this->label1->TabIndex = 21;
+			this->label1->Text = L"Horario de porción (HHmm)";
+			// 
+			// txt_horario
+			// 
+			this->txt_horario->Location = System::Drawing::Point(44, 126);
+			this->txt_horario->Name = L"txt_horario";
+			this->txt_horario->Size = System::Drawing::Size(273, 20);
+			this->txt_horario->TabIndex = 22;
+			// 
+			// btn_agregar
+			// 
+			this->btn_agregar->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(255)), static_cast<System::Int32>(static_cast<System::Byte>(128)),
+				static_cast<System::Int32>(static_cast<System::Byte>(0)));
+			this->btn_agregar->Location = System::Drawing::Point(362, 228);
+			this->btn_agregar->Name = L"btn_agregar";
+			this->btn_agregar->Size = System::Drawing::Size(135, 35);
+			this->btn_agregar->TabIndex = 23;
+			this->btn_agregar->Text = L"Agregar";
+			this->btn_agregar->UseVisualStyleBackColor = false;
+			// 
+			// btn_modificar
+			// 
+			this->btn_modificar->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(255)), static_cast<System::Int32>(static_cast<System::Byte>(128)),
+				static_cast<System::Int32>(static_cast<System::Byte>(0)));
+			this->btn_modificar->Location = System::Drawing::Point(363, 284);
+			this->btn_modificar->Name = L"btn_modificar";
+			this->btn_modificar->Size = System::Drawing::Size(135, 35);
+			this->btn_modificar->TabIndex = 24;
+			this->btn_modificar->Text = L"Modificar";
+			this->btn_modificar->UseVisualStyleBackColor = false;
+			// 
+			// btn_eliminar
+			// 
+			this->btn_eliminar->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(255)), static_cast<System::Int32>(static_cast<System::Byte>(128)),
+				static_cast<System::Int32>(static_cast<System::Byte>(0)));
+			this->btn_eliminar->Location = System::Drawing::Point(363, 343);
+			this->btn_eliminar->Name = L"btn_eliminar";
+			this->btn_eliminar->Size = System::Drawing::Size(135, 35);
+			this->btn_eliminar->TabIndex = 25;
+			this->btn_eliminar->Text = L"Eliminar";
+			this->btn_eliminar->UseVisualStyleBackColor = false;
+			// 
+			// Horario
+			// 
+			dataGridViewCellStyle2->Alignment = System::Windows::Forms::DataGridViewContentAlignment::MiddleCenter;
+			this->Horario->DefaultCellStyle = dataGridViewCellStyle2;
+			this->Horario->HeaderText = L"Horarios";
+			this->Horario->Name = L"Horario";
+			this->Horario->Width = 253;
+			// 
+			// AgregarHorarios
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(510, 443);
+			this->Controls->Add(this->btn_eliminar);
+			this->Controls->Add(this->btn_modificar);
+			this->Controls->Add(this->btn_agregar);
+			this->Controls->Add(this->txt_horario);
+			this->Controls->Add(this->label1);
+			this->Controls->Add(this->dataGridView1);
+			this->Controls->Add(this->agreg);
+			this->Controls->Add(this->pictureBox2);
+			this->Controls->Add(this->pictureBox1);
+			this->Name = L"AgregarHorarios";
+			this->Text = L"AgregarHorarios";
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox2))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->EndInit();
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
+		}
+#pragma endregion
+	};
+}
