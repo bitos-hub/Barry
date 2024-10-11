0f65bb56e09cf1b11b321ceed1d634ecfdf25593
IsabelaCG-25 <a20220357@pucp.edu.pe>
2024-10-11 14:44:40 -0500

    Se implemento MDI

--- Service/Service.h
+++ Service/Service.h
@@ -11,16 +11,18 @@ namespace ServiceBarry {
 	public:
 		static String^ BIN_FOOD_FILE_NAME = "foods.bin";
 		static String^ BIN_PET_FILE_NAME = "pets.bin";
+		static String^ BIN_USER_FILE_NAME = "users.bin";
 	private:
 		//static List<Pet^>^ lista_mascotas = gcnew List<Pet^>();
 		static List<User^>^ lista_usuarios = gcnew List<User^>();
 		static List<Pet^>^ PetsList = gcnew List<Pet^>();
 		static List<Food^>^ FoodList = gcnew List<Food^>();
 	public:
+		static void cargarUsuarios();
 		static void AddUsuario(User^);
 		static void ActualizarUsuario(User^);
 		static void EliminarUsuario(int id);
-		static User^ ConsultarUsuario(int id);
+		static User^ ConsultarUsuario(String^ UserName);
 		static List<User^>^ ConsultarTodosUsuarios();
 		static void AddPet(Pet^);
 		static void UpdatePet(Pet^);
