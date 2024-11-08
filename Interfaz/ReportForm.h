739a4c93de1df5bd59a9cee8595828412dd93c11
IsabelaCG-25 <a20220357@pucp.edu.pe>
2024-11-07 20:26:27 -0500

    se modifico el reporte

--- Interfaz/ReportForm.h
+++ Interfaz/ReportForm.h
@@ -15,8 +15,11 @@ namespace Interfaz {
 	public ref class ReportForm : public System::Windows::Forms::Form
 	{
 	public:
-		ReportForm(void)
+		int ReportType;
+	public:
+		ReportForm(int reportType)
 		{
+			ReportType = reportType;
 			InitializeComponent();
 			//
 			//TODO: agregar código de constructor aquí
@@ -34,7 +37,9 @@ namespace Interfaz {
 				delete components;
 			}
 		}
-	private: System::Windows::Forms::DataVisualization::Charting::Chart^ chart1;
+	private: System::Windows::Forms::DataVisualization::Charting::Chart^ dispensedTimeschart;
+	protected:
+
 	private: System::Windows::Forms::Label^ label1;
 	private: System::Windows::Forms::Label^ label3;
 	private: System::Windows::Forms::PictureBox^ pictureBox1;
@@ -42,7 +47,15 @@ namespace Interfaz {
 	private: System::Windows::Forms::Label^ label4;
 	private: System::Windows::Forms::ComboBox^ comboBox1;
 	private: System::Windows::Forms::PictureBox^ pictureBox2;
-	private: System::Windows::Forms::DataVisualization::Charting::Chart^ chart2;
+	private: System::Windows::Forms::DataVisualization::Charting::Chart^ WeightsCharts;
+
+	private: System::Windows::Forms::PictureBox^ pictureBox3;
+	private: System::Windows::Forms::TextBox^ txtGeneratedTime;
+
+
+
+	private: System::Windows::Forms::Label^ label6;
+	private: System::Windows::Forms::PictureBox^ pictureBox4;
 	protected:
 
 	private:
@@ -58,14 +71,14 @@ namespace Interfaz {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			System::Windows::Forms::DataVisualization::Charting::ChartArea^ chartArea1 = (gcnew System::Windows::Forms::DataVisualization::Charting::ChartArea());
-			System::Windows::Forms::DataVisualization::Charting::Legend^ legend1 = (gcnew System::Windows::Forms::DataVisualization::Charting::Legend());
-			System::Windows::Forms::DataVisualization::Charting::Series^ series1 = (gcnew System::Windows::Forms::DataVisualization::Charting::Series());
+			System::Windows::Forms::DataVisualization::Charting::ChartArea^ chartArea3 = (gcnew System::Windows::Forms::DataVisualization::Charting::ChartArea());
+			System::Windows::Forms::DataVisualization::Charting::Legend^ legend3 = (gcnew System::Windows::Forms::DataVisualization::Charting::Legend());
+			System::Windows::Forms::DataVisualization::Charting::Series^ series3 = (gcnew System::Windows::Forms::DataVisualization::Charting::Series());
 			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(ReportForm::typeid));
-			System::Windows::Forms::DataVisualization::Charting::ChartArea^ chartArea2 = (gcnew System::Windows::Forms::DataVisualization::Charting::ChartArea());
-			System::Windows::Forms::DataVisualization::Charting::Legend^ legend2 = (gcnew System::Windows::Forms::DataVisualization::Charting::Legend());
-			System::Windows::Forms::DataVisualization::Charting::Series^ series2 = (gcnew System::Windows::Forms::DataVisualization::Charting::Series());
-			this->chart1 = (gcnew System::Windows::Forms::DataVisualization::Charting::Chart());
+			System::Windows::Forms::DataVisualization::Charting::ChartArea^ chartArea4 = (gcnew System::Windows::Forms::DataVisualization::Charting::ChartArea());
+			System::Windows::Forms::DataVisualization::Charting::Legend^ legend4 = (gcnew System::Windows::Forms::DataVisualization::Charting::Legend());
+			System::Windows::Forms::DataVisualization::Charting::Series^ series4 = (gcnew System::Windows::Forms::DataVisualization::Charting::Series());
+			this->dispensedTimeschart = (gcnew System::Windows::Forms::DataVisualization::Charting::Chart());
 			this->label1 = (gcnew System::Windows::Forms::Label());
 			this->label3 = (gcnew System::Windows::Forms::Label());
 			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
@@ -73,36 +86,43 @@ namespace Interfaz {
 			this->label4 = (gcnew System::Windows::Forms::Label());
 			this->comboBox1 = (gcnew System::Windows::Forms::ComboBox());
 			this->pictureBox2 = (gcnew System::Windows::Forms::PictureBox());
-			this->chart2 = (gcnew System::Windows::Forms::DataVisualization::Charting::Chart());
-			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->chart1))->BeginInit();
+			this->WeightsCharts = (gcnew System::Windows::Forms::DataVisualization::Charting::Chart());
+			this->pictureBox3 = (gcnew System::Windows::Forms::PictureBox());
+			this->txtGeneratedTime = (gcnew System::Windows::Forms::TextBox());
+			this->label6 = (gcnew System::Windows::Forms::Label());
+			this->pictureBox4 = (gcnew System::Windows::Forms::PictureBox());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dispensedTimeschart))->BeginInit();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox2))->BeginInit();
-			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->chart2))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->WeightsCharts))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox3))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox4))->BeginInit();
 			this->SuspendLayout();
 			// 
-			// chart1
+			// dispensedTimeschart
 			// 
-			chartArea1->Name = L"ChartArea1";
-			this->chart1->ChartAreas->Add(chartArea1);
-			legend1->Name = L"Legend1";
-			this->chart1->Legends->Add(legend1);
-			this->chart1->Location = System::Drawing::Point(31, 172);
-			this->chart1->Name = L"chart1";
-			this->chart1->Palette = System::Windows::Forms::DataVisualization::Charting::ChartColorPalette::EarthTones;
-			series1->ChartArea = L"ChartArea1";
-			series1->Legend = L"Legend1";
-			series1->Name = L"Series1";
-			this->chart1->Series->Add(series1);
-			this->chart1->Size = System::Drawing::Size(300, 300);
-			this->chart1->TabIndex = 0;
-			this->chart1->Text = L"chart1";
+			chartArea3->Name = L"ChartArea1";
+			this->dispensedTimeschart->ChartAreas->Add(chartArea3);
+			legend3->Name = L"Legend1";
+			this->dispensedTimeschart->Legends->Add(legend3);
+			this->dispensedTimeschart->Location = System::Drawing::Point(351, 186);
+			this->dispensedTimeschart->Name = L"dispensedTimeschart";
+			this->dispensedTimeschart->Palette = System::Windows::Forms::DataVisualization::Charting::ChartColorPalette::EarthTones;
+			series3->ChartArea = L"ChartArea1";
+			series3->Legend = L"Legend1";
+			series3->Name = L"Series1";
+			this->dispensedTimeschart->Series->Add(series3);
+			this->dispensedTimeschart->Size = System::Drawing::Size(319, 276);
+			this->dispensedTimeschart->TabIndex = 0;
+			this->dispensedTimeschart->Text = L"chart1";
 			// 
 			// label1
 			// 
 			this->label1->AutoSize = true;
+			this->label1->BackColor = System::Drawing::Color::Gainsboro;
 			this->label1->Font = (gcnew System::Drawing::Font(L"Franklin Gothic Medium Cond", 13.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->label1->Location = System::Drawing::Point(26, 140);
+			this->label1->Location = System::Drawing::Point(346, 154);
 			this->label1->Name = L"label1";
 			this->label1->Size = System::Drawing::Size(283, 29);
 			this->label1->TabIndex = 1;
@@ -122,10 +142,10 @@ namespace Interfaz {
 			// pictureBox1
 			// 
 			this->pictureBox1->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox1.Image")));
-			this->pictureBox1->Location = System::Drawing::Point(1014, 13);
+			this->pictureBox1->Location = System::Drawing::Point(1052, 13);
 			this->pictureBox1->Margin = System::Windows::Forms::Padding(4);
 			this->pictureBox1->Name = L"pictureBox1";
-			this->pictureBox1->Size = System::Drawing::Size(151, 176);
+			this->pictureBox1->Size = System::Drawing::Size(113, 129);
 			this->pictureBox1->SizeMode = System::Windows::Forms::PictureBoxSizeMode::StretchImage;
 			this->pictureBox1->TabIndex = 15;
 			this->pictureBox1->TabStop = false;
@@ -133,9 +153,10 @@ namespace Interfaz {
 			// label2
 			// 
 			this->label2->AutoSize = true;
+			this->label2->BackColor = System::Drawing::Color::Gainsboro;
 			this->label2->Font = (gcnew System::Drawing::Font(L"Franklin Gothic Medium Cond", 13.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->label2->Location = System::Drawing::Point(386, 172);
+			this->label2->Location = System::Drawing::Point(695, 154);
 			this->label2->Name = L"label2";
 			this->label2->Size = System::Drawing::Size(292, 29);
 			this->label2->TabIndex = 17;
@@ -144,54 +165,96 @@ namespace Interfaz {
 			// label4
 			// 
 			this->label4->AutoSize = true;
+			this->label4->BackColor = System::Drawing::Color::Gainsboro;
 			this->label4->Font = (gcnew System::Drawing::Font(L"Franklin Gothic Medium Cond", 10.2F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->label4->Location = System::Drawing::Point(387, 253);
+			this->label4->Location = System::Drawing::Point(27, 272);
 			this->label4->Name = L"label4";
-			this->label4->Size = System::Drawing::Size(133, 21);
+			this->label4->Size = System::Drawing::Size(119, 21);
 			this->label4->TabIndex = 18;
-			this->label4->Text = L"Escoge la moscota:";
+			this->label4->Text = L"Elige la mascota:";
 			// 
 			// comboBox1
 			// 
 			this->comboBox1->FormattingEnabled = true;
-			this->comboBox1->Location = System::Drawing::Point(391, 319);
+			this->comboBox1->Location = System::Drawing::Point(31, 296);
 			this->comboBox1->Name = L"comboBox1";
 			this->comboBox1->Size = System::Drawing::Size(129, 24);
 			this->comboBox1->TabIndex = 19;
 			// 
 			// pictureBox2
 			// 
-			this->pictureBox2->Location = System::Drawing::Point(526, 253);
+			this->pictureBox2->BorderStyle = System::Windows::Forms::BorderStyle::Fixed3D;
+			this->pictureBox2->Location = System::Drawing::Point(31, 326);
 			this->pictureBox2->Name = L"pictureBox2";
-			this->pictureBox2->Size = System::Drawing::Size(165, 150);
+			this->pictureBox2->Size = System::Drawing::Size(129, 118);
 			this->pictureBox2->TabIndex = 20;
 			this->pictureBox2->TabStop = false;
 			// 
-			// chart2
+			// WeightsCharts
 			// 
-			chartArea2->Name = L"ChartArea1";
-			this->chart2->ChartAreas->Add(chartArea2);
-			legend2->Name = L"Legend1";
-			this->chart2->Legends->Add(legend2);
-			this->chart2->Location = System::Drawing::Point(697, 172);
-			this->chart2->Name = L"chart2";
-			this->chart2->Palette = System::Windows::Forms::DataVisualization::Charting::ChartColorPalette::EarthTones;
-			series2->ChartArea = L"ChartArea1";
-			series2->ChartType = System::Windows::Forms::DataVisualization::Charting::SeriesChartType::Line;
-			series2->Legend = L"Legend1";
-			series2->Name = L"Series1";
-			this->chart2->Series->Add(series2);
-			this->chart2->Size = System::Drawing::Size(300, 300);
-			this->chart2->TabIndex = 21;
-			this->chart2->Text = L"chart2";
+			chartArea4->Name = L"ChartArea1";
+			this->WeightsCharts->ChartAreas->Add(chartArea4);
+			legend4->Name = L"Legend1";
+			this->WeightsCharts->Legends->Add(legend4);
+			this->WeightsCharts->Location = System::Drawing::Point(700, 186);
+			this->WeightsCharts->Name = L"WeightsCharts";
+			this->WeightsCharts->Palette = System::Windows::Forms::DataVisualization::Charting::ChartColorPalette::EarthTones;
+			series4->ChartArea = L"ChartArea1";
+			series4->ChartType = System::Windows::Forms::DataVisualization::Charting::SeriesChartType::Line;
+			series4->Legend = L"Legend1";
+			series4->Name = L"Series1";
+			this->WeightsCharts->Series->Add(series4);
+			this->WeightsCharts->Size = System::Drawing::Size(305, 276);
+			this->WeightsCharts->TabIndex = 21;
+			this->WeightsCharts->Text = L"chart2";
+			// 
+			// pictureBox3
+			// 
+			this->pictureBox3->BackColor = System::Drawing::Color::Gainsboro;
+			this->pictureBox3->Location = System::Drawing::Point(12, 132);
+			this->pictureBox3->Name = L"pictureBox3";
+			this->pictureBox3->Size = System::Drawing::Size(276, 363);
+			this->pictureBox3->TabIndex = 22;
+			this->pictureBox3->TabStop = false;
+			// 
+			// txtGeneratedTime
+			// 
+			this->txtGeneratedTime->Location = System::Drawing::Point(31, 186);
+			this->txtGeneratedTime->Name = L"txtGeneratedTime";
+			this->txtGeneratedTime->Size = System::Drawing::Size(178, 22);
+			this->txtGeneratedTime->TabIndex = 26;
+			this->txtGeneratedTime->TextChanged += gcnew System::EventHandler(this, &ReportForm::textBox2_TextChanged);
+			// 
+			// label6
+			// 
+			this->label6->AutoSize = true;
+			this->label6->BackColor = System::Drawing::Color::Gainsboro;
+			this->label6->Font = (gcnew System::Drawing::Font(L"Franklin Gothic Medium Cond", 10.2F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label6->Location = System::Drawing::Point(28, 160);
+			this->label6->Name = L"label6";
+			this->label6->Size = System::Drawing::Size(100, 21);
+			this->label6->TabIndex = 25;
+			this->label6->Text = L"Dia generado:";
+			// 
+			// pictureBox4
+			// 
+			this->pictureBox4->BackColor = System::Drawing::Color::Gainsboro;
+			this->pictureBox4->Location = System::Drawing::Point(319, 132);
+			this->pictureBox4->Name = L"pictureBox4";
+			this->pictureBox4->Size = System::Drawing::Size(716, 363);
+			this->pictureBox4->TabIndex = 27;
+			this->pictureBox4->TabStop = false;
 			// 
 			// ReportForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(1178, 522);
-			this->Controls->Add(this->chart2);
+			this->ClientSize = System::Drawing::Size(1169, 571);
+			this->Controls->Add(this->txtGeneratedTime);
+			this->Controls->Add(this->label6);
+			this->Controls->Add(this->WeightsCharts);
 			this->Controls->Add(this->pictureBox2);
 			this->Controls->Add(this->comboBox1);
 			this->Controls->Add(this->label4);
@@ -199,17 +262,43 @@ namespace Interfaz {
 			this->Controls->Add(this->label3);
 			this->Controls->Add(this->pictureBox1);
 			this->Controls->Add(this->label1);
-			this->Controls->Add(this->chart1);
+			this->Controls->Add(this->dispensedTimeschart);
+			this->Controls->Add(this->pictureBox3);
+			this->Controls->Add(this->pictureBox4);
 			this->Name = L"ReportForm";
 			this->Text = L"ReportForm";
-			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->chart1))->EndInit();
+			this->Load += gcnew System::EventHandler(this, &ReportForm::ReportForm_Load);
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dispensedTimeschart))->EndInit();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->EndInit();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox2))->EndInit();
-			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->chart2))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->WeightsCharts))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox3))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox4))->EndInit();
 			this->ResumeLayout(false);
 			this->PerformLayout();
 
 		}
 #pragma endregion
-	};
+	private: System::Void ReportForm_Load(System::Object^ sender, System::EventArgs^ e) {
+		txtGeneratedTime->Text = Convert::ToString(DateTime::Now);
+		if (ReportType == 1) {//reporte semanal
+
+
+			
+			/*List<double>^ dispensedTimesList = Service::QueryAllOrders();
+			for (int i = 0; i < dispensedTimesList->Count; i++) {
+				dispensedTimeschart->Series["Veces Dispensado"]->Points->Add(dispensedTimesList[i]->TimesDispenssed);
+				dispensedTimeschart->Series["Veces Dispensado"]->Points[i]->AxisLabel = "" + dispensedTimesList[i]->Date + "\n" +
+					ordersList[i]->GeneratedBy->Name;
+				dispensedTimeschart->Series["Veces Dispensado"]->Points[i]->Label = "" + dispensedTimesList[i]->TimesDispenssed;
+			}
+			*/
+		}
+		else {
+			//reporte mensual
+		}
+	}
+private: System::Void textBox2_TextChanged(System::Object^ sender, System::EventArgs^ e) {
+}
+};
 }
